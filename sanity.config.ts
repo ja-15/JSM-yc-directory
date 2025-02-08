264d0355303b52fe425dd17f2a64718764dcd530
John <johnmichaelarenga@gmail.com>
2025-02-08 23:59:26 +1100

    sanity updated

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,9 +4,10 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision';
+import {defineConfig} from 'sanity';
+import {structureTool} from 'sanity/structure';
+import { markdownSchema } from "sanity-plugin-markdown";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
@@ -24,5 +25,6 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    markdownSchema(),
   ],
 })
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { author} from '@/sanity/schemaTypes/author';
+import {startup} from '@/sanity/schemaTypes/startup';
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [author, startup],
 }
